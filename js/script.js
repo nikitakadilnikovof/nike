a2380b32e2dfbedbf1b6b636f966cb42ea3b0208
nikitakadilnikovof <153454913+nikitakadilnikovof@users.noreply.github.com>
2024-01-09 19:56:39 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+const sneakersListDiv = document.querySelector(".sneakers-list");
+
+
 const navLinks = document.querySelector(".nav__links");
 const navLogo = document.querySelector(".nav__logo");
 const navButtons = document.querySelector(".nav__buttons");
@@ -31,9 +34,29 @@ for (let i = 0; i < 3; i++) {
     }
 }
 
-navLogo.onclick =  function () {
+navLogo.onclick = function () {
     hamburger.classList.remove("hamburger_active");
     navLinks.classList.remove("nav__links_active");
     navLogo.classList.remove("nav__logo_active");
     navButtons.classList.remove("nav__buttons_active");
+}
+
+
+const categoriesButtonAll = document.querySelectorAll(".categories__button button");
+
+for (let i = 0; i < 4; i++) {
+    categoriesButtonAll[i].onclick = function () {
+        for (let s = 0; s < 4; s++) {
+            categoriesButtonAll[s].classList.remove("categories-button_active");
+        }
+        categoriesButtonAll[i].classList.add("categories-button_active");
+    }
+}
+
+const cartLikeAll = document.querySelectorAll(".sneakers-list .sneakers-cart__like");
+
+for (let i = 0; i < cartLikeAll.length;i++){
+    cartLikeAll[i].onclick = function () {
+        cartLikeAll[i].querySelector("i").classList.toggle("fa-solid");
+    }
 }
